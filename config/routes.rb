029db1d40c4a3a1697d6f216b646aea1809b293d
mohnish-38 <jharbadeji38@gmail.com>
2022-03-06 00:03:36 +0530

    huge change

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   post 'login', to: 'session#create'
   get 'logout', to: 'session#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
 end
