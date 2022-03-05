99719c20e335a7b89149870ecace68ec867adbf9
mohnish-38 <jharbadeji38@gmail.com>
2022-03-05 22:10:49 +0530

    added login and instant msg display functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "chatroom#index"
+  get 'login', to: 'session#new'
+  post 'login', to: 'session#create'
+  get 'logout', to: 'session#destroy'
+  post 'message', to: 'messages#create'
 end
